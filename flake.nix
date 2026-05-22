850023767dec848ce79fa5aa47ad3ea1266a2b1d
Ivan Aytzhanov <aitvann@gmail.com>
2026-05-23 00:48:59 +0300

    update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Your personal jsonresume built with Nix";
 
-  inputs.jsonresume-nix.url = "github:TaserudConsulting/jsonresume-nix";
+  inputs.jsonresume-nix.url = "github:etu/jsonresume-nix";
   inputs.jsonresume-nix.inputs.flake-utils.follows = "flake-utils";
   inputs.flake-utils.url = "flake-utils";
 
@@ -13,19 +13,17 @@
     ...
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config = {
-          permittedInsecurePackages = ["qtwebkit-5.212.0-alpha4"];
-        };
-      };
+      pkgs = nixpkgs.legacyPackages.${system};
       lib = pkgs.lib;
     in {
+      # Specify formatter package for "nix fmt ." and "nix fmt . -- --check"
+      formatter = pkgs.alejandra;
+
       # Specify the builder package to use to build your resume, this
       # will decide which theme to use.
       #
       # To show available packaged themes:
-      # nix flake show github:TaserudConsulting/jsonresume-nix
+      # nix flake show github:etu/jsonresume-nix
       #
       # If you miss a theme, consider opening a pull request :)
       packages = {
@@ -38,8 +36,7 @@
         # format and copying other resources (such as images).
         default = pkgs.runCommand "resume" {} ''
           ln -s ${./resume.json} resume.json
-          ln -s ${./resources} resources
-          HOME=$(mktemp -d) ${self.packages.${system}.builder}
+          HOME=$(mktemp -d) ${lib.getExe self.packages.${system}.builder}
           mkdir $out
           cp -v resume.html $out/index.html
           # Copy other resources such as images here...
@@ -50,23 +47,16 @@
       # Allows to run a live preview server using "nix run .#live"
       apps = {
         live.type = "app";
-        live.program = builtins.toString (pkgs.writeShellScript "entr-reload" ''
-          ${self.packages.${system}.builder}
+        live.program = lib.getExe (jsonresume-nix.lib.${system}.buildLiveServer {
+          builderDerivation = self.packages.${system}.builder;
+          # Optionally override the live server implementation:
+          # liveServerPackage = <your-custom-package>;
+        });
 
-          ${lib.getExe pkgs.nodePackages.live-server} \
-            --watch=resume.html --open=resume.html --wait=300 &
-
-          printf "\n%s" resume.{toml,nix,json} |
-            ${lib.getExe pkgs.xe} -s 'test -f "$1" && echo "$1"' |
-            ${lib.getExe pkgs.entr} -p ${self.packages.${system}.builder}
-        '');
-      };
-
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          # wkhtmltopdf
-          # puppeteer-cli
-        ];
+        print.type = "app";
+        print.program = lib.getExe (jsonresume-nix.lib.${system}.buildPrintToPdf {
+          builderDerivation = self.packages.${system}.builder;
+        });
       };
     })
     // {inherit inputs;};
